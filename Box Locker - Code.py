81330d5fc672c5c47972b1329ad39296742dab78
it57070047 <s7070047@kmitl.ac.th>
2014-11-17 16:33:48 +0700

    Make Buttons
    Increase & Decrease Button for make Box size N x N

--- Box Locker - Code.py
+++ Box Locker - Code.py
@@ -1,1 +1,25 @@
 ''' Box Locker '''
+import Tkinter
+import sys
+
+root = Tkinter.Tk()
+root.geometry("800x600")
+root.title("Box Locker")
+
+counter = Tkinter.IntVar()
+
+def onClick_Increase(event=None):
+    counter.set(counter.get() + 1)
+
+def onClick_Decrease(event=None):
+    counter.set(counter.get() - 1)
+
+Tkinter.Label(root, textvariable=counter).pack()
+Tkinter.Button(root, text="Increase", command=onClick_Increase, fg="dark green", bg = "white").pack()
+Tkinter.Button(root, text="Decrease", command=onClick_Decrease, fg="dark green", bg = "white").pack()
+
+    
+
+
+
+root.mainloop()
