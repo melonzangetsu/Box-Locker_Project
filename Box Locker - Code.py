c237355f34ede5510ff363a2fde5de659fb7d46c
it57070047 <s7070047@kmitl.ac.th>
2014-11-18 22:55:29 +0700

    Add 60 Boxs, logo, Fix size window and button

--- Box Locker - Code.py
+++ Box Locker - Code.py
@@ -1,25 +1,49 @@
 ''' Box Locker '''
-import Tkinter
+from Tkinter import *
 import sys
 
-root = Tkinter.Tk()
-root.geometry("800x600")
+root = Tk()
+root.geometry("800x630")
 root.title("Box Locker")
+#------------------------------------------------------------------------------------------------------#
+''' Zone LOGO PROGRAM '''
+photo = PhotoImage(file = "logo.gif")
+label = Label(image = photo)
+label.image = photo # keep a reference!
+label.place(x = 350, y = 5)
 
-counter = Tkinter.IntVar()
 
+#------------------------------------------------------------------------------------------------------#
+''' Zone Menu '''
+
+
+
+
+#------------------------------------------------------------------------------------------------------#
+''' Button Skip '''
+counter = IntVar()
 def onClick_Increase(event=None):
     counter.set(counter.get() + 1)
 
 def onClick_Decrease(event=None):
-    counter.set(counter.get() - 1)
+    if counter.get() >= 1:
+        counter.set(counter.get() - 1)
 
-Tkinter.Label(root, textvariable=counter).pack()
-Tkinter.Button(root, text="Increase", command=onClick_Increase, fg="dark green", bg = "white").pack()
-Tkinter.Button(root, text="Decrease", command=onClick_Decrease, fg="dark green", bg = "white").pack()
+Label(root, textvariable=counter).place(x = 645, y = 55)
+Button(root, text="<- ", command=onClick_Decrease, fg="red", bg = "black").place(x = 625, y = 80)
+Button(root, text=" ->", command=onClick_Increase, fg="red", bg = "black").place(x = 650, y = 80)
 
-    
-
-
-
+#------------------------------------------------------------------------------------------------------#
+''' Zone Blocks '''
+block = 1
+right = 0
+down = 65
+for i in xrange(6):
+    down += 80
+    right = 0
+    for j in xrange(10):
+        Button(root, text = block).place(x = right, y = down, width = 80, height = 80)
+        right += 80
+        block += 1
+#------------------------------------------------------------------------------------------------------#
 root.mainloop()
