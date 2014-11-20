cb521bc4f7fef1007e17b13ead2b8cfc3952b45a
melonzangetsu <s7070046@kmitl.ac.th>
2014-11-20 12:02:18 +0700

    add text color in buttons

--- Box Locker - Code.py
+++ Box Locker - Code.py
@@ -1,49 +1,21 @@
 ''' Box Locker '''
-from Tkinter import *
+import Tkinter
 import sys
 
-root = Tk()
-root.geometry("800x630")
+root = Tkinter.Tk()
+root.geometry("800x600")
 root.title("Box Locker")
-#------------------------------------------------------------------------------------------------------#
-''' Zone LOGO PROGRAM '''
-photo = PhotoImage(file = "logo.gif")
-label = Label(image = photo)
-label.image = photo # keep a reference!
-label.place(x = 350, y = 5)
 
+counter = Tkinter.IntVar()
 
-#------------------------------------------------------------------------------------------------------#
-''' Zone Menu '''
-
-
-
-
-#------------------------------------------------------------------------------------------------------#
-''' Button Skip '''
-counter = IntVar()
 def onClick_Increase(event=None):
     counter.set(counter.get() + 1)
 
 def onClick_Decrease(event=None):
-    if counter.get() >= 1:
-        counter.set(counter.get() - 1)
+    counter.set(counter.get() - 1)
 
-Label(root, textvariable=counter).place(x = 645, y = 55)
-Button(root, text="<- ", command=onClick_Decrease, fg="red", bg = "black").place(x = 625, y = 80)
-Button(root, text=" ->", command=onClick_Increase, fg="red", bg = "black").place(x = 650, y = 80)
+Tkinter.Label(root, textvariable=counter).pack()
+Tkinter.Button(root, text="Increase", command=onClick_Increase, fg="black", bg = "white").pack()
+Tkinter.Button(root, text="Decrease", command=onClick_Decrease, fg="red", bg = "white").pack()
 
-#------------------------------------------------------------------------------------------------------#
-''' Zone Blocks '''
-block = 1
-right = 0
-down = 65
-for i in xrange(6):
-    down += 80
-    right = 0
-    for j in xrange(10):
-        Button(root, text = block).place(x = right, y = down, width = 80, height = 80)
-        right += 80
-        block += 1
-#------------------------------------------------------------------------------------------------------#
-root.mainloop()
+    
